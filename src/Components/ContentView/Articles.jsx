5258e431a74dc7e08bc948d9c942dfeb684d0442
corkesw <home@stephencorke.com>
2021-10-18 15:36:27 +0100

    add link to individual article

--- src/Components/ContentView/Articles.jsx
+++ src/Components/ContentView/Articles.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { getArticles } from '../../utils/api';
 import '../../Css/Articles.css'
+import { Link } from 'react-router-dom';
 
 const Articles = () => {
     
@@ -18,8 +19,10 @@ const Articles = () => {
             {articles.map((article) => {
                 return (
                 <div className="article__div" key={article.article_id}>
+                    <Link to={`/article/${article.article_id}`}>
                 <p>{article.title}</p>
                 <p>Topic: {article.topic.toUpperCase()} Author: {article.author} </p>
+                </Link>
                 </div>)
             })}
         </section>
