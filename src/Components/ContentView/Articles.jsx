1457324b0a693ceec803e360996f147fbab8d981
corkesw <home@stephencorke.com>
2021-10-19 11:02:09 +0100

    add date and styling to articles view

--- src/Components/ContentView/Articles.jsx
+++ src/Components/ContentView/Articles.jsx
@@ -5,33 +5,42 @@ import { Link } from "react-router-dom";
 import { useParams } from "react-router";
 
 const Articles = () => {
-
   const [articles, setArticles] = useState([]);
-  const {search} = useParams()
-  console.log(search)
+  const { search } = useParams();
+  console.log(search);
 
   useEffect(() => {
-    getArticles(search).then((articlesFromApi) => {
-      setArticles(articlesFromApi);
-    }).catch(err => console.log(err, "<<<<<<<<<<"));
+    getArticles(search)
+      .then((articlesFromApi) => {
+        setArticles(articlesFromApi);
+      })
+      .catch((err) => console.log(err, "<<<<<<<<<<"));
   }, [search]);
 
   return (
     <section className="articles">
-      {articles.length ? 
-      articles.map((article) => {
-        return (
-          <div className="article__div" key={article.article_id}>
-            <Link to={`/article/${article.article_id}`}>
-              <p>{article.title}</p>
-              <p>
-                Topic: {article.topic.toUpperCase()} Author: {article.author}{" "}
-              </p>
-            </Link>
-          </div>
-        );
-      }) :
-      <p>No articles found</p> }
+      {articles.length ? (
+        articles.map((article) => {
+          return (
+            <div className="article__div" key={article.article_id}>
+              <Link
+                className="article--link"
+                to={`/article/${article.article_id}`}
+              >
+                <p className="article--title">{article.title}</p>
+                <p className="article--details">
+                  Topic: {article.topic.toUpperCase()} Author: {article.author}{" "}
+                </p>
+                <p className="article--details">
+                  Date posted:{Date(article.created_at)}
+                </p>
+              </Link>
+            </div>
+          );
+        })
+      ) : (
+        <p>No articles found</p>
+      )}
     </section>
   );
 };
