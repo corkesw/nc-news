5b4d35563d371d9d0f04be3806684db679734faa
corkesw <home@stephencorke.com>
2021-10-20 11:37:21 +0100

    add error handling to comment submit

--- src/Components/ContentView/CommentAdd.jsx
+++ src/Components/ContentView/CommentAdd.jsx
@@ -7,12 +7,17 @@ const CommentAdd = ({user, article_id, addComment, viewComments}) => {
     
     const [newCommentInput, setNewCommentInput] = useState('')
     const [newComment, setNewComment] = useState(null)
+    const [err, setErr] = useState(null)
+
+    console.log(newCommentInput, '<<<<NCI')
     
     const handleCommentSubmit = (e) => {
         e.preventDefault()
+        setErr(null)
         setNewComment(newCommentInput);
         postComments(newCommentInput, user, article_id)
-        .catch(err => console.dir(err, '<<<<<<<<'))
+        .then(()=> {setNewCommentInput('')})
+        .catch(error => setErr(error))
       }
   return (
     <>
@@ -29,7 +34,7 @@ const CommentAdd = ({user, article_id, addComment, viewComments}) => {
           <button className="comment--submit">Submit</button>
         </form>
       ) : null}
-      
+      {err ? <p className="errormessage">Connection error, please try again later</p> : null}
       {newComment && viewComments ? (
         <>
           <p>{newComment}</p>
