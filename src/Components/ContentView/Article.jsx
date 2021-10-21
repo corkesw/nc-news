f4db9dc28046c084272c058fc2e3404e37c7ec55
corkesw <home@stephencorke.com>
2021-10-21 14:02:30 +0100

    amend styling to vote button connection error

--- src/Components/ContentView/Article.jsx
+++ src/Components/ContentView/Article.jsx
@@ -11,16 +11,16 @@ const Article = ({ user }) => {
   const { article_id } = useParams(); // selected article id
   const [votes, setVotes] = useState(); // votes for selected article
   const [err, setErr] = useState(null); // error state if vote fails
-  const [loadErr, setLoadErr] = useState(false)
+  const [loadErr, setLoadErr] = useState(false);
   const [viewComments, setViewComments] = useState(false); // toggle between comment view and change text on comment button : Comments / Hide comments
   const [addComment, setAddComment] = useState(false); //toggle comment box open/closed
   const { on, loading, reset } = useLoading();
-  const [commentChange, setCommentChange] = useState(false) // update if comment added or deleted
-  const commentAdded = useLoading()
+  const [commentChange, setCommentChange] = useState(false); // update if comment added or deleted
+  const commentAdded = useLoading();
 
   useEffect(() => {
     loading(true);
-    setLoadErr(false)
+    setLoadErr(false);
     getArticle(article_id)
       .then((articleFromApi) => {
         const topic = articleFromApi.topic.toUpperCase();
@@ -31,8 +31,8 @@ const Article = ({ user }) => {
       })
       .catch((error) => {
         if (error) {
-          setLoadErr('Article not found')
-          reset()
+          setLoadErr("Article not found");
+          reset();
         }
       });
   }, [article_id, loading]); // don't add reset!
@@ -54,84 +54,81 @@ const Article = ({ user }) => {
 
   return (
     <>
-    {loadErr? <p className="errormessage errorcentre">Article not found</p> :
-    <>
-      {on ? (
-        <div className="spinner">
-          <div className="lds-facebook">
-            <div></div>
-            <div></div>
-            <div></div>
-          </div>
-        </div>
+      {loadErr ? (
+        <p className="errormessage errorcentre">Article not found</p>
       ) : (
-        <section className="article__div">
-          <p className="article--title">{article.title}</p>
-          <p className="article--details">
-            Topic: {article.topic} Author: {article.author}
-          </p>
-          <p className="article--details">
-            Date posted:{" "}
-            {`${new Date(article.created_at).getDate()}/${
-              new Date(article.created_at).getMonth() + 1
-            }/${new Date(article.created_at).getFullYear()}`}
-          </p>
-          <p>{article.body}</p>
-          <button type="button" onClick={handleVoteClick}>
-            Votes: {votes}
-          </button>
-          {err ? <p className="errormessage">{err}</p> : null}
-
-          {viewComments ? (
-            <button type="button" onClick={handleCommentClick}>
-              Hide Comments
-            </button>
+        <>
+          {on ? (
+            <div className="spinner">
+              <div className="lds-facebook">
+                <div></div>
+                <div></div>
+                <div></div>
+              </div>
+            </div>
           ) : (
-            <button type="button" onClick={handleCommentClick}>
-              Comments
-            </button>
+            <section className="article__div">
+              <p className="article--title">{article.title}</p>
+              <p className="article--details">
+                Topic: {article.topic} Author: {article.author}
+              </p>
+              <p className="article--details">
+                Date posted:{" "}
+                {`${new Date(article.created_at).getDate()}/${
+                  new Date(article.created_at).getMonth() + 1
+                }/${new Date(article.created_at).getFullYear()}`}
+              </p>
+              <p>{article.body}</p>
+              <button type="button" onClick={handleVoteClick}>
+                Votes: {votes}
+              </button>
+
+              {viewComments ? (
+                <button type="button" onClick={handleCommentClick}>
+                  Hide Comments
+                </button>
+              ) : (
+                <button type="button" onClick={handleCommentClick}>
+                  Comments
+                </button>
+              )}
+
+              {!addComment ? (
+                <button
+                  onClick={() => {
+                    setAddComment(true);
+                    commentAdded.reset();
+                  }}
+                >
+                  Add comment
+                </button>
+              ) : null}
+              {err ? <p className="errormessage comment">{err}</p> : null}
+              <CommentAdd
+                user={user}
+                article_id={article_id}
+                addComment={addComment}
+                viewComments={viewComments}
+                commentChange={commentChange}
+                setCommentChange={setCommentChange}
+                setViewComments={setViewComments}
+                commentAdded={commentAdded}
+                setAddComment={setAddComment}
+              />
+
+              <Comments
+                user={user}
+                article_id={article_id}
+                setViewComments={setViewComments}
+                viewComments={viewComments}
+                commentChange={commentChange}
+                setCommentChange={setCommentChange}
+              />
+            </section>
           )}
-
-          {!addComment ? (
-            <button
-              onClick={() => {
-                setAddComment(true);
-                commentAdded.reset()
-              }}
-            >
-              Add comment
-            </button>
-          ) : null
-          
-          
-            
-          }
-
-          <CommentAdd
-            user={user}
-            article_id={article_id}
-            addComment={addComment}
-            viewComments={viewComments}
-            commentChange={commentChange}
-            setCommentChange={setCommentChange}
-            setViewComments={setViewComments}
-            commentAdded={commentAdded}
-            setAddComment={setAddComment}
-          />
-
-          <Comments
-            user={user}
-            article_id={article_id}
-            setViewComments={setViewComments}
-            viewComments={viewComments}
-            commentChange={commentChange}
-            setCommentChange={setCommentChange}
-          />
-        </section>
+        </>
       )}
     </>
-}
-</>
   );
 };
 
