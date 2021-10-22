0cae67d95aeb087af23cc540ff1ba19d663be08a
corkesw <home@stephencorke.com>
2021-10-22 13:14:58 +0100

    change css which was preventing header button click to open menu

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,9 +4,15 @@ import "../Css/Header.css"
 
 const Header = ({setMenuOpen, menuOpen}) => {
     console.log(menuOpen)
+    const menuChange = (e) => {
+        // e.preventDefault()
+        console.log('hi')
+        setMenuOpen(true)
+    }   
+    
     return (
         <header className="header">
-            <button className="burger vanish" onClick={() => setMenuOpen(true)}>=</button>
+            <button type="button" className="burger vanish" onClick={menuChange}>=</button>
             <h1 className="title">NC NEWS!</h1>
         </header>
     );
