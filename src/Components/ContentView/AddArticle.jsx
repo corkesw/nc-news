7517b514a4f69e76cfbfc339c693507d5a92d460
corkesw <home@stephencorke.com>
2021-10-22 08:20:55 +0100

    add input suggestion for topic in add article view

--- src/Components/ContentView/AddArticle.jsx
+++ src/Components/ContentView/AddArticle.jsx
@@ -1,13 +1,41 @@
-import React from 'react';
-import '../../Css/Articles.css'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Hint } from "react-autocomplete-hint";
+import "../../Css/Articles.css";
+import { getTopics } from "../../utils/api";
 
-const AddArticle = () => {
-    console.log('in add')
-    return (
-        <section className="article__div">
-            <p className="article--title centre"> Add Article</p>
-        </section>
-    );
-};
+function AddArticle() {
+  const [hintData, setHintData] = useState([]);
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    getTopics().then((topicsFromApi) => {
+      const topics = topicsFromApi.map((topic) => {
+        return topic.slug;
+      });
+      setHintData(topics);
+    });
+  }, []);
+  
+  return (
+    <>
+      <section className="article__div">
+        <p className="article--title">Add article</p>
+        <form className="article__form">
+          <label htmlFor="topic">Topic</label>
+          <Hint id="topic" options={hintData} allowTabFill>
+            <input
+              className="input-with-hint"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            />
+          </Hint>
+          <label htmlFor="title">Title</label>
+          <input id="title" type="text" />
+        </form>
+      </section>
+    </>
+  );
+}
 
 export default AddArticle;
