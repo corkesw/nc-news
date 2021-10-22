da54175eeacc7bfa496e4e9f27f1a6bd38e091db
corkesw <home@stephencorke.com>
2021-10-22 12:08:21 +0100

    add change to login styling

--- src/Components/ContentView/Login.jsx
+++ src/Components/ContentView/Login.jsx
@@ -1,38 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { useContext } from "react";
 import { UserContext } from "../../Contexts/User";
-import "../../Css/login.css"
-import { getUsers } from '../../utils/api';
+import "../../Css/Login.css";
+import { getUsers } from "../../utils/api";
 
 const Login = () => {
-    const {user, setUser} = useContext(UserContext)
-    const [userInput, setUserInput] = useState('jessjelly')
-    const [err, setErr] = useState(null)
-    const handleLogin = (e) => {
-        e.preventDefault()
-        setErr(null)
-        getUsers(userInput)
-        .then((res) => {
-            if (res) {
-                setUser(userInput)
-                localStorage.setItem('loggedInUser', userInput)
-                setUserInput('')
-            }
-            else setErr('User not found')
-        })
-    }
+  const { user, setUser } = useContext(UserContext);
+  const [userInput, setUserInput] = useState("jessjelly");
+  const [err, setErr] = useState(null);
+  const handleLogin = (e) => {
+    e.preventDefault();
+    setErr(null);
+    getUsers(userInput).then((res) => {
+      if (res) {
+        setUser(userInput);
+        localStorage.setItem("loggedInUser", userInput);
+        setUserInput("");
+      } else setErr("User not found");
+    });
+  };
 
-    return (
-        <div className="login">
-            <form onSubmit={handleLogin}>
-                <label htmlFor="username" name="username"></label>
-                <input onChange={ (e) => setUserInput(e.target.value)} value={userInput}type="text" name="username" placeholder="username"></input>
-                <button>Login</button>
-            {err ? <p className="errormessage">{err}</p> : null}
-            {user? <p>You are logged in as {user}</p> : null}
-            </form>
-        </div>
-    );
+  return (
+    <div className="login">
+      <form onSubmit={handleLogin}>
+        <label htmlFor="username" name="username"></label>
+        <input
+          onChange={(e) => {
+            setErr(null);
+            setUserInput(e.target.value);
+          }}
+          value={userInput}
+          type="text"
+          name="username"
+          placeholder="username"
+        ></input>
+        <button>Login</button>
+        <br />
+        {err ? <p className="errormessage">{err}</p> : null}
+        {user ? <p>You are logged in as {user}</p> : null}
+      </form>
+    </div>
+  );
 };
 
 export default Login;
