19b7abcd3538a2a31f49fc2332380f32e0a5c685
corkesw <home@stephencorke.com>
2021-10-22 12:02:26 +0100

    fix logout error

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,37 +1,50 @@
-import React from 'react';
-import "../Css/Menu.css"
+import React from "react";
+import "../Css/Menu.css";
 import { useContext } from "react";
 import { UserContext } from "../Contexts/User";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-const Menu = ({menuOpen, setMenuOpen}) => {
-    const {user, setUser} = useContext(UserContext)
-    const handleLogout = () => {
-        setUser(null)
-        localStorage.removeItem('loggedInUser')
-    }
-    return (
-        <div className={`sidenav menuopen${menuOpen}`}>
-            <button className="closemenu vanish" onClick={ () => setMenuOpen(false)}>X</button>
-            
-            {user ? 
-            <>
-            <p className="logindetails">Logged in as</p>
-            <p className="logindetails user">  {user}</p>
-            <Link to="/add"><button>Add article</button></Link>
-            <Link to="/login"><button onClick={() =>handleLogout} onClick={() => setMenuOpen(false)}className="logout">Logout</button></Link>
-            </>
-            : 
-            
-            <Link to="/login"><button onClick={() => setMenuOpen(false)}className="logout">Login</button></Link>
-            
-            
-            }
-        
+const Menu = ({ menuOpen, setMenuOpen }) => {
+  const { user, setUser } = useContext(UserContext);
+  const handleLogout = () => {
+    console.log("in handle logout");
+    setUser(null);
+    localStorage.removeItem("loggedInUser");
+  };
+  return (
+    <div className={`sidenav menuopen${menuOpen}`}>
+      <button className="closemenu vanish" onClick={() => setMenuOpen(false)}>
+        X
+      </button>
 
-            
-        </div>
-    );
+      {user ? (
+        <>
+          <p className="logindetails">Logged in as</p>
+          <p className="logindetails user"> {user}</p>
+          <Link to="/add">
+            <button>Add article</button>
+          </Link>
+          <Link to="/login">
+            <button
+              onClick={() => {
+                handleLogout()
+                setMenuOpen(false)}
+              }
+              className="logout"
+            >
+              Logout
+            </button>
+          </Link>
+        </>
+      ) : (
+        <Link to="/login">
+          <button onClick={() => setMenuOpen(false)} className="logout">
+            Login
+          </button>
+        </Link>
+      )}
+    </div>
+  );
 };
 
 export default Menu;
